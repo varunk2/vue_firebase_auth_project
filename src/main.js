90fec0a390ba359f1c97d6690b528c034d4a43b5
Varun Sharma <varunk22110@gmail.com>
2021-08-04 22:17:36 +0530

    Created Login and Register views.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDc91jhCqi9Levn-TqI02sNDfY9DWSy1h8",
+    authDomain: "vue-auth-project-d91ae.firebaseapp.com",
+    projectId: "vue-auth-project-d91ae",
+    storageBucket: "vue-auth-project-d91ae.appspot.com",
+    messagingSenderId: "34945054604",
+    appId: "1:34945054604:web:bffa9a28a5b513c4a688a9"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 createApp(App).use(router).mount('#app')
